1826a74cd4bf35ccb3a2d0de500c0f48f9965314
krulunio <49376929+krulunio@users.noreply.github.com>
2023-04-12 20:08:00 +0200

    replace_values update

--- src/replace_values.py
+++ src/replace_values.py
@@ -2,18 +2,22 @@ import json
 import os
 
 
-def resolve_tags(values_and_tags, tags):
+def check_dict(default_value, name, key):
     try:
-        values_list = []
-        for value_or_tag in values_and_tags:
-            if value_or_tag[0] == '#':
-                for value_from_tag in tags[value_or_tag[1:]]:
-                    values_list.append(value_from_tag)
-            else:
-                values_list.append(value_or_tag)
-        return values_list
+        return name[key]
     except:
-        return []
+        return default_value
+
+
+def resolve_tags(values_and_tags, tags):
+    values_list = []
+    for value_or_tag in values_and_tags:
+        if value_or_tag[0] == '#':
+            for value_from_tag in tags[value_or_tag[1:]]:
+                values_list.append(value_from_tag)
+        else:
+            values_list.append(value_or_tag)
+    return values_list
 
 
 def check_exclusions(key_parts, exclusions):
@@ -35,12 +39,12 @@ def parse_action(current_pattern, current_output):
         case "base":
             return current_pattern["value"]
         case "begin":
-            return current_pattern["value"] + current_pattern["separator"] + current_output
+            return current_pattern["value"] + check_dict("", current_pattern, "separator") + current_output
         case "end":
-            return current_output + current_pattern["separator"] + current_pattern["value"]
+            return current_output + check_dict("", current_pattern, "separator") + current_pattern["value"]
         case "replace":
             return current_output.replace(current_pattern["replace_what"], current_pattern["replace_with"])
-    return current_output
+    raise ValueError("No action named '{}'!".format(current_pattern["action"]))
 
 
 def parse_patterns(loaded_patterns, tags, current_key, current_value):
@@ -48,10 +52,8 @@ def parse_patterns(loaded_patterns, tags, current_key, current_value):
     for current_pattern in loaded_patterns:
         key_parts = current_key.split(".")[-1].split("_")
         inclusions = resolve_tags(current_pattern["inclusions"], tags)
-        try:
-            exclusions = resolve_tags(current_pattern["exclusions"], tags)
-        except:
-            exclusions = []
+        exclusions = check_dict([], current_pattern, "exclusions")
+        exclusions = resolve_tags(exclusions, tags)
         if check_inclusions(key_parts, inclusions) and not check_exclusions(key_parts, exclusions):
             output_value = parse_action(current_pattern, output_value)
     return output_value
@@ -60,13 +62,14 @@ def parse_patterns(loaded_patterns, tags, current_key, current_value):
 def run(config_file):
     for config_group in json.load(open(config_file, "r")).values():
         pattern_file = json.load(open(config_group["pattern_file"], "r"))
-        key_root = pattern_file["key_root"]
         loaded_patterns = pattern_file["patterns"]
         input_file = json.load(open(config_group["input_file"], "r"))
+        key_root = check_dict("", pattern_file, "key_root")
+        tags = check_dict({}, pattern_file, "tags")
         output_object = {}
         for key, value in input_file.items():
             output_object[key] = value
             if key.startswith(key_root):
-                output_object[key] = parse_patterns(loaded_patterns, pattern_file["tags"], key, value)
+                output_object[key] = parse_patterns(loaded_patterns, tags, key, value)
         os.makedirs(os.path.dirname(config_group["output_file"]), exist_ok=True)
         json.dump(output_object, open(config_group["output_file"], "w"), indent="\t")
